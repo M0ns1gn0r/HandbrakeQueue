3ae708c2c6b91a0c07a0da06c5930c69fb52388e
M0ns1gn0r <rfever@gmail.com>
2016-12-05 20:43:09 +0100

    Add full support of Typescript.

---
+++ src/App.js
@@ -0,0 +1,5 @@
+"use strict";
+var electron_1 = require('electron');
+var Main_1 = require('./Main');
+Main_1.default.main(electron_1.app, electron_1.BrowserWindow);
+//# sourceMappingURL=App.js.map
---
+++ src/Main.ts
@@ -0,0 +1,44 @@
+import { BrowserWindow } from 'electron';
+
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow: typeof BrowserWindow;
+
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
+
+    private static onClose() {
+        // Dereference the window object.
+        Main.mainWindow = null;
+    }
+
+    private static onReady() {
+        const w = new Main.BrowserWindow({width: 800, height: 600});
+        
+        Main.mainWindow = w;
+        w.loadURL('file://' + __dirname + '/index.html');
+        w.on('closed', Main.onClose);
+
+        // Open the DevTools.        
+        w.webContents.openDevTools();
+
+        // Disable main menu.
+        w.setMenu(null);
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow){
+        // we pass the Electron.App object and the 
+        // Electron.BrowserWindow into this function
+        // so this class has no dependencies.  This
+        // makes the code easier to write tests for
+        Main.BrowserWindow = browserWindow;
+
+        Main.application = app;
+        app.on('window-all-closed', Main.onWindowAllClosed);
+        app.on('ready', Main.onReady);
+    }
+}
