b892688387e3dd6aab067a4928d6040a863f9962
M0ns1gn0r <rfever@gmail.com>
2016-12-25 12:34:36 +0100

    Restructure the application.

--- scripts/Main.ts
+++ scripts/Main.ts
@@ -1,38 +1,44 @@
-"use strict";
-var Main = (function () {
-    function Main() {
-    }
-    Main.onWindowAllClosed = function () {
+import { BrowserWindow } from 'electron';
+
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow: typeof BrowserWindow;
+
+    private static onWindowAllClosed() {
         if (process.platform !== 'darwin') {
             Main.application.quit();
         }
-    };
-    Main.onClose = function () {
+    }
+
+    private static onClose() {
         // Dereference the window object.
         Main.mainWindow = null;
-    };
-    Main.onReady = function () {
-        var w = new Main.BrowserWindow({ width: 800, height: 600 });
+    }
+
+    private static onReady() {
+        const w = new Main.BrowserWindow({width: 800, height: 600});
+        
         Main.mainWindow = w;
-        w.loadURL('file://' + __dirname + '/index.html');
+        w.loadURL('file://' + __dirname + '/../html/index.html');
         w.on('closed', Main.onClose);
+
         // Open the DevTools.        
         w.webContents.openDevTools();
+
         // Disable main menu.
         w.setMenu(null);
-    };
-    Main.main = function (app, browserWindow) {
-        // we pass the Electron.App object and the 
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow){
+        // we pass the Electron.App object and the
         // Electron.BrowserWindow into this function
         // so this class has no dependencies.  This
         // makes the code easier to write tests for
         Main.BrowserWindow = browserWindow;
+
         Main.application = app;
         app.on('window-all-closed', Main.onWindowAllClosed);
         app.on('ready', Main.onReady);
-    };
-    return Main;
-}());
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Main;
-//# sourceMappingURL=Main.js.map
+    }
+}
--- src/App.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var electron_1 = require('electron');
-var Main_1 = require('./Main');
-Main_1.default.main(electron_1.app, electron_1.BrowserWindow);
-//# sourceMappingURL=App.js.map
