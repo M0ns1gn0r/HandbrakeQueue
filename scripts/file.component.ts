779e054515e52162157325cc813b0273ece4b80d
M0ns1gn0r <rfever@gmail.com>
2017-01-15 14:05:51 +0100

    Retrieve the file user navigated to.

--- scripts/file.component.ts
+++ scripts/file.component.ts
@@ -1,5 +1,6 @@
-import * as fs from 'fs';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FileService } from './file.service';
 import { FileInfo } from './file-info';
 
 @Component({
@@ -9,5 +10,27 @@ import { FileInfo } from './file-info';
     <p>The file will be here</p>
   </div>`
 })
-export class FileComponent {
+export class FileComponent implements OnInit {
+  file: FileInfo;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fileService: FileService) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(ps => {
+      const idxString = ps['idx'];
+      if (!idxString) {
+        throw new Error('The "idx" parameter was not specified.');
+      }
+      const idx = +idxString;
+
+      if (this.fileService.files.length <= idx) {
+        throw new Error('The file with the passed "idx" parameter does not exist.');
+      }
+
+      this.file = this.fileService.files[idx];
+    });
+  }
 }
