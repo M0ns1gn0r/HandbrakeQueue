93f67d0e0ef5b499157604e5cf1407b74b172d8a
M0ns1gn0r <rfever@gmail.com>
2018-02-14 19:17:31 +0100

    Adapt queue output to Handbrake 1.07 requirements.

--- scripts/queue.service.ts
+++ scripts/queue.service.ts
@@ -28,145 +28,157 @@ export class QueueService {
             <EndPoint>${segment.lastSecond || 999999}</EndPoint>`;
     }
 
-    private getRotation(rotate: number) {
-        if (!rotate) {
-            return '';
-        }
-
-        let rotateValue;
-        if (rotate === 90) {
-            rotateValue = 4;
-        } else if (rotate === -90) {
-            rotateValue = 7;
-        } else {
-            rotateValue = 3; // 180 degrees.
-        }
-
-        return `<ExtraAdvancedArguments>, --rotate=${rotateValue}</ExtraAdvancedArguments>`;
-    }
-
     private fileToXml(file: FileInfo) {
         const c = file.config;
         const targetPath = c.targetPath;
         const videoSegment = this.getVideoSegment(c.segment);
-        const rotation = this.getRotation(c.rotate);
+        const rotation = c.rotate === -90 ? 270 : c.rotate;
         // TODO: take into account the selected preset.
 
+        // The schema corresponds to Handbrake 1.07.
         return `
     <QueueTask>
-        <Status>Waiting</Status>
-        <Task>
-            <OptimizeMP4>true</OptimizeMP4>
-            <IPod5GSupport>false</IPod5GSupport>
-            <VideoBitrate>2500</VideoBitrate>
-            <AudioTracks>
-                <AudioTrack>
-                    <Bitrate>128</Bitrate>
-                    <DRC>0</DRC>
-                    <IsDefault>false</IsDefault>
-                    <Encoder>AacPassthru</Encoder>
-                    <Gain>0</Gain>
-                    <MixDown>DolbyProLogicII</MixDown>
-                    <SampleRate>0</SampleRate>
-                    <SampleRateDisplayValue>Auto</SampleRateDisplayValue>
-                    <ScannedTrack>
-                        <TrackNumber>1</TrackNumber>
-                        <Language>English</Language>
-                        <LanguageCode>eng</LanguageCode>
-                        <Description>English (AAC) (2.0 ch)</Description>
-                        <Format />
-                        <SampleRate>48000</SampleRate>
-                        <Bitrate>127498</Bitrate>
-                    </ScannedTrack>
-                    <TrackName />
-                </AudioTrack>
-            </AudioTracks>
-            <AllowedPassthruOptions>
-                <AudioAllowAACPass>true</AudioAllowAACPass>
-                <AudioAllowAC3Pass>true</AudioAllowAC3Pass>
-                <AudioAllowDTSHDPass>true</AudioAllowDTSHDPass>
-                <AudioAllowDTSPass>true</AudioAllowDTSPass>
-                <AudioAllowMP3Pass>true</AudioAllowMP3Pass>
-                <AudioEncoderFallback>Ac3</AudioEncoderFallback>
-            </AllowedPassthruOptions>
-            <SubtitleTracks />
-            <ChapterNames>
-                <ChapterMarker>
-                    <ChapterNumber>1</ChapterNumber>
-                    <Duration />
-                    <ChapterName>Chapter 1</ChapterName>
-                </ChapterMarker>
-            </ChapterNames>
-            <Source>${file.path}</Source>
-            <Title>1</Title>
-            <Angle>1</Angle>
-            ${videoSegment}
-            <Destination>${targetPath}</Destination>
-            <OutputFormat>Mp4</OutputFormat>
-            <Width>1920</Width>
-            <Height>0</Height>
-            <MaxWidth xsi:nil="true" />
-            <MaxHeight xsi:nil="true" />
-            <Cropping>
-                <Top>0</Top>
-                <Bottom>0</Bottom>
-                <Left>0</Left>
-                <Right>0</Right>
-            </Cropping>
-            <HasCropping>false</HasCropping>
-            <Anamorphic>Loose</Anamorphic>
-            <DisplayWidth xsi:nil="true" />
-            <KeepDisplayAspect>false</KeepDisplayAspect>
-            <PixelAspectX>0</PixelAspectX>
-            <PixelAspectY>0</PixelAspectY>
-            <Modulus>2</Modulus>
-            <Deinterlace>Off</Deinterlace>
-            <Decomb>Default</Decomb>
-            <Detelecine>Off</Detelecine>
-            <Denoise>Off</Denoise>
-            <DenoisePreset>Weak</DenoisePreset>
-            <DenoiseTune>None</DenoiseTune>
-            <Deblock>4</Deblock>
-            <Grayscale>false</Grayscale>
-            <VideoEncodeRateType>AverageBitrate</VideoEncodeRateType>
-            <VideoEncoder>X264</VideoEncoder>
-            <FramerateMode>CFR</FramerateMode>
-            <Quality>20</Quality>
-            <TwoPass>true</TwoPass>
-            <TurboFirstPass>false</TurboFirstPass>
-            <Framerate xsi:nil="true" />
-            <IncludeChapterMarkers>true</IncludeChapterMarkers>
-            <AdvancedEncoderOptions />
-            <X264Preset>VerySlow</X264Preset>
-            <QsvPreset>Balanced</QsvPreset>
-            <H264Profile>High</H264Profile>
-            <H264Level>4.1</H264Level>
-            <X264Tune>None</X264Tune>
-            <FastDecode>false</FastDecode>
-            ${rotation}
-            <X265Preset>VeryFast</X265Preset>
-            <H265Profile>None</H265Profile>
-            <X265Tune>None</X265Tune>
-            <PreviewStartAt xsi:nil="true" />
-            <PreviewDuration xsi:nil="true" />
-            <IsPreviewEncode>false</IsPreviewEncode>
-            <PreviewEncodeDuration>0</PreviewEncodeDuration>
-            <ShowAdvancedTab>false</ShowAdvancedTab>
-        </Task>
-        <Configuration>
-            <IsLoggingEnabled>true</IsLoggingEnabled>
-            <IsDvdNavDisabled>false</IsDvdNavDisabled>
-            <DisableQuickSyncDecoding>false</DisableQuickSyncDecoding>
-            <EnableDxva>false</EnableDxva>
-            <ScalingMode>Lanczos</ScalingMode>
-            <PreviewScanCount>10</PreviewScanCount>
-            <Verbosity>1</Verbosity>
-            <MinScanDuration>10</MinScanDuration>
-            <ProcessPriority>Below Normal</ProcessPriority>
-            <SaveLogToCopyDirectory>false</SaveLogToCopyDirectory>
-            <SaveLogWithVideo>false</SaveLogWithVideo>
-            <SaveLogCopyDirectory />
-        </Configuration>
+      <ScannedSourcePath>${file.path}</ScannedSourcePath>
+      <Status>Waiting</Status>
+      <Task>
+        <Source>${file.path}</Source>
+        <Title>1</Title>
+        <Angle>1</Angle>
+        ${videoSegment}
+        <Destination>${targetPath}</Destination>
+        <OutputFormat>Mp4</OutputFormat>
+        <OptimizeMP4>true</OptimizeMP4>
+        <IPod5GSupport>false</IPod5GSupport>
+        <Width>1920</Width>
+        <Height>1080</Height>
+        <MaxWidth xsi:nil="true" />
+        <MaxHeight xsi:nil="true" />
+        <Cropping>
+          <Top>0</Top>
+          <Bottom>0</Bottom>
+          <Left>0</Left>
+          <Right>0</Right>
+        </Cropping>
+        <HasCropping>false</HasCropping>
+        <Anamorphic>Automatic</Anamorphic>
+        <DisplayWidth xsi:nil="true" />
+        <KeepDisplayAspect>true</KeepDisplayAspect>
+        <PixelAspectX>1</PixelAspectX>
+        <PixelAspectY>1</PixelAspectY>
+        <Modulus>2</Modulus>
+        <DeinterlaceFilter>Decomb</DeinterlaceFilter>
+        <Deinterlace>Default</Deinterlace>
+        <CustomDeinterlace />
+        <Decomb>Default</Decomb>
+        <CombDetect>Default</CombDetect>
+        <Detelecine>Off</Detelecine>
+        <CustomDetelecine />
+        <Denoise>Off</Denoise>
+        <DenoisePreset>Light</DenoisePreset>
+        <DenoiseTune>None</DenoiseTune>
+        <CustomDenoise />
+        <Deblock>4</Deblock>
+        <Grayscale>false</Grayscale>
+        <Rotation>${rotation}</Rotation>
+        <FlipVideo>false</FlipVideo>
+        <VideoEncodeRateType>AverageBitrate</VideoEncodeRateType>
+        <VideoEncoder>X264</VideoEncoder>
+        <FramerateMode>CFR</FramerateMode>
+        <Quality>22</Quality>
+        <VideoBitrate>2500</VideoBitrate>
+        <TwoPass>true</TwoPass>
+        <TurboFirstPass>false</TurboFirstPass>
+        <Framerate xsi:nil="true" />
+        <AudioTracks>
+          <AudioTrack>
+            <DRC>0</DRC>
+            <Gain>0</Gain>
+            <Encoder>AacPassthru</Encoder>
+            <SampleRate>0</SampleRate>
+            <EncoderRateType>Bitrate</EncoderRateType>
+            <Bitrate>160</Bitrate>
+            <Quality>-1</Quality>
+            <IsDefault>false</IsDefault>
+            <SampleRateDisplayValue>Auto</SampleRateDisplayValue>
+            <ScannedTrack>
+              <TrackNumber>1</TrackNumber>
+              <Language>English</Language>
+              <LanguageCode>eng</LanguageCode>
+              <Description>English (AAC) (2.0 ch)</Description>
+              <Codec>65536</Codec>
+              <SampleRate>48000</SampleRate>
+              <Bitrate>127579</Bitrate>
+              <ChannelLayout>3</ChannelLayout>
+            </ScannedTrack>
+          </AudioTrack>
+        </AudioTracks>
+        <AllowedPassthruOptions>
+          <AudioAllowAACPass>true</AudioAllowAACPass>
+          <AudioAllowAC3Pass>true</AudioAllowAC3Pass>
+          <AudioAllowDTSHDPass>true</AudioAllowDTSHDPass>
+          <AudioAllowDTSPass>true</AudioAllowDTSPass>
+          <AudioAllowMP3Pass>true</AudioAllowMP3Pass>
+          <AudioAllowTrueHDPass>true</AudioAllowTrueHDPass>
+          <AudioAllowFlacPass>true</AudioAllowFlacPass>
+          <AudioAllowEAC3Pass>true</AudioAllowEAC3Pass>
+          <AudioEncoderFallback>Ac3</AudioEncoderFallback>
+        </AllowedPassthruOptions>
+        <SubtitleTracks>
+          <SubtitleTrack>
+            <Burned>true</Burned>
+            <Default>false</Default>
+            <Forced>true</Forced>
+            <SourceTrack>
+              <SourceId>0</SourceId>
+              <TrackNumber>0</TrackNumber>
+              <Language>Foreign Audio Search (Bitmap)</Language>
+              <SubtitleType>ForeignAudioSearch</SubtitleType>
+            </SourceTrack>
+            <SrtOffset>0</SrtOffset>
+            <SubtitleType>VobSub</SubtitleType>
+          </SubtitleTrack>
+        </SubtitleTracks>
+        <IncludeChapterMarkers>true</IncludeChapterMarkers>
+        <ChapterNames>
+          <ChapterMarker>
+            <ChapterNumber>1</ChapterNumber>
+            <Duration />
+            <ChapterName>Chapter 1</ChapterName>
+          </ChapterMarker>
+        </ChapterNames>
+        <AdvancedEncoderOptions />
+        <VideoProfile>
+          <DisplayName>High</DisplayName>
+          <ShortName>high</ShortName>
+        </VideoProfile>
+        <VideoLevel>
+          <DisplayName>4.1</DisplayName>
+          <ShortName>4.1</ShortName>
+        </VideoLevel>
+        <VideoPreset>
+          <DisplayName>VerySlow</DisplayName>
+          <ShortName>veryslow</ShortName>
+        </VideoPreset>
+        <VideoTunes />
+        ${rotation}
+        <ShowAdvancedTab>false</ShowAdvancedTab>
+        <MetaData />
+        <IsPreviewEncode>false</IsPreviewEncode>
+        <PreviewEncodeDuration xsi:nil="true" />
+        <PreviewEncodeStartAt xsi:nil="true" />
+      </Task>
+      <Configuration>
+        <IsDvdNavDisabled>false</IsDvdNavDisabled>
+        <DisableQuickSyncDecoding>false</DisableQuickSyncDecoding>
+        <UseQSVDecodeForNonQSVEnc>false</UseQSVDecodeForNonQSVEnc>
+        <ScalingMode>Lanczos</ScalingMode>
+        <PreviewScanCount>10</PreviewScanCount>
+        <Verbosity>1</Verbosity>
+        <MinScanDuration>10</MinScanDuration>
+        <SaveLogToCopyDirectory>false</SaveLogToCopyDirectory>
+        <SaveLogWithVideo>false</SaveLogWithVideo>
+        <SaveLogCopyDirectory />
+      </Configuration>
     </QueueTask>`;
     }
 }
